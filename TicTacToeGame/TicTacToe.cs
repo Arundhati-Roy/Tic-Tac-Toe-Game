a2391ffe3a7a99b12f9c8fd4682ec348e8e54d5e
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-10-10 16:16:03 +0530

    [AR] Display Board

--- TicTacToeGame/TicTacToe.cs
+++ TicTacToeGame/TicTacToe.cs
@@ -7,10 +7,11 @@ namespace TicTacToeGame
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Tic Tac Toe Game!");
-            CreateBoard();
+            char[] board=CreateBoard();
             chooseInput();
+            showBoard(board);
         }
-        public static void CreateBoard()
+        public static char[] CreateBoard()
         {
             char[] board = new char[10];
             for (int i=1;i<=10;i++)
@@ -18,12 +19,21 @@ namespace TicTacToeGame
                 board[i-1] = ' ';
             }
             Console.WriteLine("Board Created");
+            return board;
         }
         public static void chooseInput()
         {
             Console.WriteLine("Choose X or O");
             char input = Convert.ToChar(Console.ReadLine());
             Console.WriteLine("You chose " + input);
+        }
+        public static void showBoard(char[] board)
+        {
+            Console.WriteLine("\n "+"|"+board[0]+"|"+board[1]+ "|" + board[2]);
+            Console.WriteLine("-------");
+            Console.WriteLine(" " + "|" + board[3] + "|" + board[4] + "|" + board[5]);
+            Console.WriteLine("-------");
+            Console.WriteLine(" " + "|" + board[6] + "|" + board[7] + "|" + board[8]);
         }
     }
 }
