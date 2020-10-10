9329ff8a4d4fe4e685fd59d7375043ebce360721
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-10-10 15:52:31 +0530

    [AR] Choose X or O

--- TicTacToeGame/TicTacToe.cs
+++ TicTacToeGame/TicTacToe.cs
@@ -13,17 +13,29 @@ namespace TicTacToeGame
         public static void CreateBoard()
         {
             char[] board = new char[10];
-            for (int i=1;i<=10;i++)
+            for (int i = 1; i <= 10; i++)
             {
-                board[i-1] = ' ';
+                board[i - 1] = ' ';
             }
             Console.WriteLine("Board Created");
         }
         public static void chooseInput()
         {
             Console.WriteLine("Choose X or O");
-            char input = Convert.ToChar(Console.ReadLine());
-            Console.WriteLine("You chose " + input);
+            string input = (Console.ReadLine());
+            bool val = true;
+            while (val)
+            {
+                Console.WriteLine("Choose X or O");
+                input = (Console.ReadLine());
+                if (char.ToUpper(input[0]) == 'X' | char.ToUpper(input[0]) == 'O')
+                {
+                    Console.WriteLine("You chose " + char.ToUpper(input[0]));
+                    val = false;
+                }
+                else
+                    Console.WriteLine("Invalid Input");
+            }
         }
     }
 }
