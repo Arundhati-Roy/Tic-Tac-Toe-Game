82b06deee8e895a1351c376fc3ece9e54fa345b5
Arundhati-Roy <arundhati.roy1998@gmail.com>
2020-10-10 14:56:42 +0530

    [AR] Refactor. Empty Board Created

--- TicTacToeGame/Program.cs
+++ TicTacToeGame/Program.cs
@@ -7,6 +7,16 @@ namespace TicTacToeGame
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Tic Tac Toe Game!");
+            CreateBoard();
+        }
+        public static void CreateBoard()
+        {
+            char[] board = new char[10];
+            for (int i=1;i<=10;i++)
+            {
+                board[i-1] = ' ';
+            }
+            Console.WriteLine("Board Created");
         }
     }
 }
